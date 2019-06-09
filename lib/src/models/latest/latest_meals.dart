7a3bbb29ee10d0193739d3f31ee5d76d06358347
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-06-10 01:00:23 +0700

    Buat class model untuk endpoint latest meals

---
+++ lib/src/models/latest/latest_meals.dart
@@ -0,0 +1,139 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'latest_meals.g.dart';
+
+@JsonSerializable()
+class LatestMeals {
+  @JsonKey(name: "meals")
+  List<LatestMealsItem> latestMealsItems;
+
+  LatestMeals({this.latestMealsItems});
+
+  @override
+  String toString() {
+    return 'LatestMeals{latestMealsItems: $latestMealsItems}';
+  }
+
+  factory LatestMeals.fromJson(Map<String, dynamic> json) =>
+      _$LatestMealsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LatestMealsToJson(this);
+}
+
+@JsonSerializable()
+class LatestMealsItem {
+  String idMeal;
+  String strMeal;
+  String strDrinkAlternate;
+  String strCategory;
+  String strArea;
+  String strInstructions;
+  String strMealThumb;
+  String strTags;
+  String strYoutube;
+  String strIngredient1;
+  String strIngredient2;
+  String strIngredient3;
+  String strIngredient4;
+  String strIngredient5;
+  String strIngredient6;
+  String strIngredient7;
+  String strIngredient8;
+  String strIngredient9;
+  String strIngredient10;
+  String strIngredient11;
+  String strIngredient12;
+  String strIngredient13;
+  String strIngredient14;
+  String strIngredient15;
+  String strIngredient16;
+  String strIngredient17;
+  String strIngredient18;
+  String strIngredient19;
+  String strIngredient20;
+  String strMeasure1;
+  String strMeasure2;
+  String strMeasure3;
+  String strMeasure4;
+  String strMeasure5;
+  String strMeasure6;
+  String strMeasure7;
+  String strMeasure8;
+  String strMeasure9;
+  String strMeasure10;
+  String strMeasure11;
+  String strMeasure12;
+  String strMeasure13;
+  String strMeasure14;
+  String strMeasure15;
+  String strMeasure16;
+  String strMeasure17;
+  String strMeasure18;
+  String strMeasure19;
+  String strMeasure20;
+  String strSource;
+  String dateModified;
+
+  LatestMealsItem(
+      {this.idMeal,
+      this.strMeal,
+      this.strDrinkAlternate,
+      this.strCategory,
+      this.strArea,
+      this.strInstructions,
+      this.strMealThumb,
+      this.strTags,
+      this.strYoutube,
+      this.strIngredient1,
+      this.strIngredient2,
+      this.strIngredient3,
+      this.strIngredient4,
+      this.strIngredient5,
+      this.strIngredient6,
+      this.strIngredient7,
+      this.strIngredient8,
+      this.strIngredient9,
+      this.strIngredient10,
+      this.strIngredient11,
+      this.strIngredient12,
+      this.strIngredient13,
+      this.strIngredient14,
+      this.strIngredient15,
+      this.strIngredient16,
+      this.strIngredient17,
+      this.strIngredient18,
+      this.strIngredient19,
+      this.strIngredient20,
+      this.strMeasure1,
+      this.strMeasure2,
+      this.strMeasure3,
+      this.strMeasure4,
+      this.strMeasure5,
+      this.strMeasure6,
+      this.strMeasure7,
+      this.strMeasure8,
+      this.strMeasure9,
+      this.strMeasure10,
+      this.strMeasure11,
+      this.strMeasure12,
+      this.strMeasure13,
+      this.strMeasure14,
+      this.strMeasure15,
+      this.strMeasure16,
+      this.strMeasure17,
+      this.strMeasure18,
+      this.strMeasure19,
+      this.strMeasure20,
+      this.strSource,
+      this.dateModified});
+
+
+  @override
+  String toString() {
+    return 'LatestMealsItem{idMeal: $idMeal, strMeal: $strMeal, strDrinkAlternate: $strDrinkAlternate, strCategory: $strCategory, strArea: $strArea, strInstructions: $strInstructions, strMealThumb: $strMealThumb, strTags: $strTags, strYoutube: $strYoutube, strIngredient1: $strIngredient1, strIngredient2: $strIngredient2, strIngredient3: $strIngredient3, strIngredient4: $strIngredient4, strIngredient5: $strIngredient5, strIngredient6: $strIngredient6, strIngredient7: $strIngredient7, strIngredient8: $strIngredient8, strIngredient9: $strIngredient9, strIngredient10: $strIngredient10, strIngredient11: $strIngredient11, strIngredient12: $strIngredient12, strIngredient13: $strIngredient13, strIngredient14: $strIngredient14, strIngredient15: $strIngredient15, strIngredient16: $strIngredient16, strIngredient17: $strIngredient17, strIngredient18: $strIngredient18, strIngredient19: $strIngredient19, strIngredient20: $strIngredient20, strMeasure1: $strMeasure1, strMeasure2: $strMeasure2, strMeasure3: $strMeasure3, strMeasure4: $strMeasure4, strMeasure5: $strMeasure5, strMeasure6: $strMeasure6, strMeasure7: $strMeasure7, strMeasure8: $strMeasure8, strMeasure9: $strMeasure9, strMeasure10: $strMeasure10, strMeasure11: $strMeasure11, strMeasure12: $strMeasure12, strMeasure13: $strMeasure13, strMeasure14: $strMeasure14, strMeasure15: $strMeasure15, strMeasure16: $strMeasure16, strMeasure17: $strMeasure17, strMeasure18: $strMeasure18, strMeasure19: $strMeasure19, strMeasure20: $strMeasure20, strSource: $strSource, dateModified: $dateModified}';
+  }
+
+  factory LatestMealsItem.fromJson(Map<String, dynamic> json) => _$LatestMealsItemFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LatestMealsItemToJson(this);
+
+}

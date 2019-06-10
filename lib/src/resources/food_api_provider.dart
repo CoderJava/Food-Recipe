cd6176a3b34e231a432410b7cabe3ed3f25a5c45
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-06-10 12:26:42 +0700

    Tambahkan endpoint get categories di food api provider

--- lib/src/resources/food_api_provider.dart
+++ lib/src/resources/food_api_provider.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:food_recipe/src/models/area/area_meals.dart';
+import 'package:food_recipe/src/models/categories/categories.dart';
 
 class FoodApiProvider {
   Dio dio = Dio();
@@ -13,4 +14,14 @@ class FoodApiProvider {
       throw Exception("Failed to get area meals");
     }
   }
+
+  Future<Categories> getCategories() async {
+    final response = await dio.get("$_baseUrl/api/json/v1/1/categories.php");
+    if (response.statusCode == 200) {
+      return Categories.fromJson(response.data);
+    } else {
+      throw Exception("Failed to get categories");
+    }
+  }
+
 }

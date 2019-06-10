ec6726de93ab070abcf93efe7f90f1b82d7b13a8
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-06-10 12:55:10 +0700

    Tambahkan endpoint get area meals di food api provider

--- lib/src/resources/food_api_provider.dart
+++ lib/src/resources/food_api_provider.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:food_recipe/src/models/area/area_meals.dart';
 import 'package:food_recipe/src/models/categories/categories.dart';
 import 'package:food_recipe/src/models/latest/latest_meals.dart';
 import 'package:food_recipe/src/models/randommeals/random_meals.dart';
@@ -33,4 +34,14 @@ class FoodApiProvider {
       throw Exception("Failed to get latest meals");
     }
   }
+  
+  Future<AreaMeals> getAreaMeals() async {
+    final response = await dio.get("$_baseUrl/api/json/v1/1/list.php?a=list");
+    if (response.statusCode == 200) {
+      return AreaMeals.fromJson(response.data);
+    } else {
+      throw Exception("Failed to get area meals");
+    }
+  }
+  
 }

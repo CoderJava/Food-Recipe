fe8cba4555b040e69af623d5952932f7e729b9d4
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-06-10 13:29:20 +0700

    Buat class repository
    Buat class repository untuk mempusatkan proses pemanggilan endpoint dan database

---
+++ lib/src/resources/repository.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:food_recipe/src/models/area/area_meals.dart';
+import 'package:food_recipe/src/models/categories/categories.dart';
+import 'package:food_recipe/src/models/filterarea/filter_area_meals.dart';
+import 'package:food_recipe/src/models/filtercategories/filter_categories.dart';
+import 'package:food_recipe/src/models/latest/latest_meals.dart';
+import 'package:food_recipe/src/models/lookupmealsbyid/lookup_meals_by_id.dart';
+import 'package:food_recipe/src/models/randommeals/random_meals.dart';
+import 'package:food_recipe/src/models/searchmeals/search_meals.dart';
+
+import 'food_api_provider.dart';
+
+class Repository {
+  final foodApiProvider = FoodApiProvider();
+
+  Future<RandomMeals> getRandomMeals() => foodApiProvider.getRandomMeals();
+
+  Future<Categories> getCategories() => foodApiProvider.getCategories();
+
+  Future<LatestMeals> getLatestMeals() => foodApiProvider.getLatestMeals();
+
+  Future<AreaMeals> getAreaMeals() => foodApiProvider.getAreaMeals();
+
+  Future<FilterCategories> getFilterByCategories(String category) =>
+      foodApiProvider.getFilterByCategories(category);
+
+  Future<FilterAreaMeals> getFilterByArea(String area) =>
+      foodApiProvider.getFilterByArea(area);
+
+  Future<LookupMealsById> getLookupMealsById(String id) =>
+      foodApiProvider.getLookupMealsById(id);
+
+  Future<SearchMeals> getSearchMealsByKeyword(String keyword) =>
+      foodApiProvider.getSearchMealsByKeyword(keyword);
+}

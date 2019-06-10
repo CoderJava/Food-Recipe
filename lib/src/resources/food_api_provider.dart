08bcd75250912243fc7bde92f41445642b9c371d
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-06-10 12:52:53 +0700

    Perbaikan typo nama endpoint dari getAreaMeals menjadi getRandomMeals di food api provider

--- lib/src/resources/food_api_provider.dart
+++ lib/src/resources/food_api_provider.dart
@@ -1,18 +1,18 @@
 import 'package:dio/dio.dart';
-import 'package:food_recipe/src/models/area/area_meals.dart';
 import 'package:food_recipe/src/models/categories/categories.dart';
 import 'package:food_recipe/src/models/latest/latest_meals.dart';
+import 'package:food_recipe/src/models/randommeals/random_meals.dart';
 
 class FoodApiProvider {
   Dio dio = Dio();
   final _baseUrl = "https://www.themealdb.com";
 
-  Future<AreaMeals> getAreaMeals() async {
+  Future<RandomMeals> getRandomMeals() async {
     final response = await dio.get("$_baseUrl/api/json/v1/1/random.php");
     if (response.statusCode == 200) {
-      return AreaMeals.fromJson(response.data);
+      return RandomMeals.fromJson(response.data);
     } else {
-      throw Exception("Failed to get area meals");
+      throw Exception("Failed to get random meals");
     }
   }
 
@@ -33,5 +33,4 @@ class FoodApiProvider {
       throw Exception("Failed to get latest meals");
     }
   }
-
 }

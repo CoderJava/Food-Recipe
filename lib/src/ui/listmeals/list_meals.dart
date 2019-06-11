9e52922899eded3156671c20ebdb29c1198e7f37
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-06-11 17:24:05 +0700

    Buat widget arrow back menu di UI List Meals

--- lib/src/ui/listmeals/list_meals.dart
+++ lib/src/ui/listmeals/list_meals.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,26 +13,44 @@ class _ListMealsState extends State<ListMeals> {
   Widget build(BuildContext context) {
     var mediaQuery = MediaQuery.of(context);
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          _buildWidgetBackgroundCircle(mediaQuery),
-        ],
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Stack(
+          children: <Widget>[
+            _buildWidgetBackgroundCircle(mediaQuery),
+            _buildWidgetArrowBackMenu(),
+          ],
+        ),
       ),
     );
   }
 
-  Positioned _buildWidgetBackgroundCircle(MediaQueryData mediaQuery) {
-    return Positioned(
-          top: -100.0,
-          left: -100.0,
-          child: Container(
-            width: mediaQuery.size.height / 2.2,
-            height: mediaQuery.size.height / 2.2,
-            decoration: BoxDecoration(
-              color: Color(0x3FE8364B),
-              shape: BoxShape.circle,
+  Widget _buildWidgetArrowBackMenu() {
+    return SafeArea(
+            minimum: EdgeInsets.symmetric(horizontal: 16.0),
+            child: CircleAvatar(
+              child: Platform.isIOS
+                  ? Icon(Icons.arrow_back_ios, color: Colors.black)
+                  : Icon(Icons.arrow_back, color: Colors.black),
+              maxRadius: 24.0,
+              backgroundColor: Colors.white,
             ),
-          ),
-        );
+          );
+  }
+
+  Widget _buildWidgetBackgroundCircle(MediaQueryData mediaQuery) {
+    return Positioned(
+      top: -100.0,
+      left: -100.0,
+      child: Container(
+        width: mediaQuery.size.height / 2.2,
+        height: mediaQuery.size.height / 2.2,
+        decoration: BoxDecoration(
+          color: Color(0x3FE8364B),
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
   }
 }

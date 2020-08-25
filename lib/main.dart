00ebbfed962752a3f1ff84cc491705936713f901
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2020-08-25 23:49:22 +0700

    Initial commit v2.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
-import 'src/app.dart';
-
 void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Coming Soon'),
+      ),
+    );
+  }
+}
--- lib/values/color_assets.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ColorAssets {
-  static final Color primarySwatchColor = Color(0xFFE8364B);
-  static final Color accentColor = Color(0xFFEE8F3B);
-  static final Color primaryTextColor = Colors.grey[900];
-  static final Color secondColorCategoryItem = Color(0xFF3FD231);
-  static final Color thirdColorCategoryItem = Color(0xFFF03F7D);
-}
